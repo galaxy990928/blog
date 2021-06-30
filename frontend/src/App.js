d873d9016e866b43ae5a99599ca7971b90230aae
galaxy999280 <galaxy990928@naver.com>
2021-06-30 13:27:28 +0900

    change import to CDN

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import MainPage from './pages/MainPage'
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
